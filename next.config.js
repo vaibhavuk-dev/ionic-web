ab728c492bc0066e274b0520eb0c68756b47d3b3
Vaibhav <thisisvuk1@gmail.com>
2025-01-26 22:31:33 +0530

    add redirect for chlorine dioxide generator manufacturers page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 module.exports = {
-    
+  async redirects() {
+    return [
+      {
+        source: '/chlorine-dioxide-generator-manufacturers.html',
+        destination: '/products/water-treatment/genox-smart-digital-chlorine-dioxide-generator',
+        permanent: true, // Use 301 permanent redirect
+      },
+      // Add more redirects as needed
+    ];
+  },
   eslint: {
-  	    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true,
   }
 };
